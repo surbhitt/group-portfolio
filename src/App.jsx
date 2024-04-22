d071308b917cb02a69034035f55974e55f9a6533
surbhitt <surbhit.thakur@gmail.com>
2024-04-23 00:24:17 +0530

    first commit with tailwind shadcn

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import { Button } from './components/ui/button';
+
+function App() {
+    return (
+        <>
+
+            <Button variant="destructive">Here</Button> <br /><br />
+            <div className="text-red-500">Hello world</div>
+        </>
+    )
+}
+
+export default App
